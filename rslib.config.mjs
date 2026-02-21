265cd1fcbf5f8e6225033d961e9073356781ef71
Adam Liang <hello@adamliang.dev>
2026-02-20 23:09:37 -0500

    chore: updated manifest
    - allows for fp-ts-stream/Stream and fp-ts-stream/AsyncStream

--- rslib.config.mjs
+++ rslib.config.mjs
@@ -1,36 +1,31 @@
 import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
 /** @type {import('@rslib/core').RslibConfig} */
 export default {
-	lib: [
-		{
-			format: "esm",
-			syntax: "es2022",
-			autoExternal: true,
-			name: "[name].esm.mjs",
-			dts: { bundle: true, tsgo: true },
-		},
-		{
-			format: "cjs",
-			syntax: "es2022",
-			autoExternal: true,
-			name: "[name].cjs.mjs",
-			dts: { bundle: true, tsgo: true },
-		},
-	],
-	input: "src/index.ts",
-	source: {
-		tsconfigPath: "./tsconfig.app.json",
-	},
-	output: {
-		target: "web",
-		minify: true,
-		sourcemap: true,
-	},
-	performance: {
-		chunkSplit: {
-			strategy: "split-by-experience",
-		},
-	},
-	plugins: [pluginNodePolyfill()],
-	external: ["fp-ts/*"],
+  lib: [
+    {
+      format: "esm",
+      syntax: "es2022",
+      autoExternal: true,
+      dts: { bundle: false, tsgo: true },
+    },
+  ],
+  source: {
+    entry: {
+      AsyncStream: "src/AsyncStream/index.ts",
+      Stream: "src/Stream/index.ts",
+    },
+    tsconfigPath: "./tsconfig.app.json",
+  },
+  output: {
+    target: "web",
+    minify: true,
+    sourcemap: true,
+  },
+  performance: {
+    chunkSplit: {
+      strategy: "split-by-experience",
+    },
+  },
+  plugins: [pluginNodePolyfill()],
+  external: ["fp-ts/*"],
 };
